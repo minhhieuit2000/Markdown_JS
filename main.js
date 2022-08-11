15ec0bda673ea6bc77274ae46fc4c935b84aeaac
Minh Hiếu IT <108927256+minhhieuit2000@users.noreply.github.com>
2022-08-11 16:55:24 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-var weight, height, BMI;
-weight = prompt("Nhập cân nặng:");
-height = prompt("Nhập chiều cao:");
-BMI = weight / (height * height);
-alert("Chỉ số BMI của bạn là " + BMI);
-// alert("Chỉ số BMI của bạn là " + Math.round(BMI*100)/100); // Làm tròn số sau dấu phẩy
+
